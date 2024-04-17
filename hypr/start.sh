a89eba00832d72e3b8a140156d4ed2f0fb26c40d
michael <mihailomijacic@gmail.com>
2024-04-17 14:10:57 +0200

    messing around

--- hypr/start.sh
+++ hypr/start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 swww init &
-#EDIT THIS LINE FOR CUSTOM IMAGE
-swww img ~/.config/Europa.png &
+swww img ~/Pictures/Wallpapers/Europa.png &
 nm-applet --indicator &
 blueman-applet &
 waybar &
